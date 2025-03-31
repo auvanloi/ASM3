75bba1e890afc97c93c64a43bc115b547d12c31f
Âu Văn Lợi <loiavfx57470@funix.edu.vn>
2025-03-31 10:12:39 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+"use strict";
+function checkEmail(email) {
+  const regex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  email = document.querySelector(".email").value;
+  if (regex.test(email)) {
+    document.querySelector(".form-info").classList.toggle("hidden");
+    document.querySelector(".info-content").classList.toggle("hidden");
+  } else {
+    alert("Email không hợp lệ vui lòng thử lại !");
+  }
+}
+
+for (let i = 0; i < 6; i++) {
+  document.querySelector(`.btn${i}`).addEventListener("click", showhidden);
+  let swBtnLabel = document.querySelector(`.btn${i}`).textContent;
+  function showhidden() {
+    document.querySelector(`.intro-item${i}`).classList.toggle("hidden");
+    document.querySelector(`.btn${i}`).textContent =
+      swBtnLabel === "▼ VIEW MORE"
+        ? (swBtnLabel = "▲ VIEW LESS")
+        : (swBtnLabel = "▼ VIEW MORE");
+  }
+}
+document.querySelector(".btn-submid").addEventListener("click", checkEmail);
+email.addEventListener("keypress", function keyEnter(a) {
+  if (a.key === "Enter") checkEmail();
+});
